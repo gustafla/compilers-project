d71771335bf692b0dc837a1cb93fb78fe414b572
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-03-03 11:45:22 +0200

    Add basic end-to-end testing

---
+++ tests/programs.rs
@@ -0,0 +1,82 @@
+use serde::Deserialize;
+use std::{
+    ffi::OsStr,
+    fs::{self, Permissions},
+    io::Write,
+    path::Path,
+    process::{Command, Stdio},
+};
+
+#[derive(Deserialize)]
+struct Json {
+    stdout: Option<String>,
+    stdin: Option<String>,
+    error: Option<bool>,
+}
+
+#[test]
+fn compile_and_run_programs() {
+    let config = compilers_project::Config { verbose: true };
+    for file in fs::read_dir(Path::new(env!("CARGO_MANIFEST_DIR")).join("programs")).unwrap() {
+        let Ok(file) = file else {
+            continue;
+        };
+        if !file.metadata().is_ok_and(|m| m.is_file()) {
+            continue;
+        }
+        if file.path().extension() == Some(OsStr::new("json")) {
+            continue;
+        }
+
+        // Load config for the program test case
+        let conf = fs::read_to_string(file.path().with_extension("json")).unwrap();
+        let json: Json = serde_json::from_str(&conf).unwrap();
+
+        // Compile and check status
+        let code = fs::read_to_string(file.path()).unwrap();
+        let elf = match (
+            compilers_project::compile(&code, &config),
+            json.error.unwrap_or(false),
+        ) {
+            (Ok(elf), false) => elf,
+            (Ok(_), true) => panic!("Program compiled, but error was expected"),
+            (Err(e), false) => panic!("Unexpected compiler error: {e}"),
+            (Err(_), true) => return,
+        };
+
+        // Create temp binary to run
+        let mut tempfile = tempfile::NamedTempFile::new().unwrap();
+        #[cfg(target_family = "unix")]
+        {
+            use std::os::unix::fs::PermissionsExt;
+            let file = tempfile.as_file();
+            let mut mode = file.metadata().unwrap().permissions().mode();
+            mode |= 0o111;
+            file.set_permissions(Permissions::from_mode(mode)).unwrap();
+        }
+        tempfile.write_all(&elf).unwrap();
+        tempfile.flush().unwrap();
+        let (_, binary) = tempfile.keep().unwrap();
+
+        // Run and feed stdin
+        let mut process = Command::new(binary)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+        if let (Some(input), Some(stdin)) = (&json.stdin, &mut process.stdin) {
+            stdin.write_all(input.as_bytes()).unwrap();
+        }
+
+        // Check the output
+        let output = process.wait_with_output().unwrap();
+        if let Some(stdout) = json.stdout {
+            if output.stdout != stdout.as_bytes() {
+                eprintln!("Expected: {stdout}");
+                eprintln!("Output: {}", String::from_utf8_lossy(&output.stdout));
+                eprintln!("In file: {}", file.path().display());
+                panic!("Executable produced output which does not match expected");
+            }
+        }
+    }
+}
